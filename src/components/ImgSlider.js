d3e1ff64e2666f6447992dc95f55979b3ee00d05
Mahfuz-13 <mahfuzulh14@gmail.com>
2022-10-21 22:03:27 +0600

    commit

---
+++ src/components/ImgSlider.js
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+const ImgSlider = (props) => {
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+  return (
+    <Carousel {...settings}>
+      <div>
+        <h1>Je</h1>
+      </div>
+      <div>
+        <h1>Je22</h1>
+      </div>
+      <div>
+        <h1>Je22</h1>
+      </div>
+    </Carousel>
+  );
+};
+
+const Carousel = styled(Slider)`
+  margin-top: 20px;
+
+  & > button {
+    opacity: 0;
+    height: 100%;
+    width: 5vw;
+    z-index: 1;
+    &:hover {
+      opacity: 1;
+      transition: opacity 0.2s ease 0s;
+    }
+  }
+
+  ul li button {
+    &:before {
+      font-size: 10px;
+      color: rgb(150, 158, 171);
+    }
+  }
+`;
+
+export default ImgSlider;
